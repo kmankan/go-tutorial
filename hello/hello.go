8197c0b29bd3edd1d145c98a4d7c7ebe2203541a
mahlen <malink027@gmail.com>
2024-10-11 12:48:50 -0400

    tutorial

---
+++ hello/hello.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"example.com/greetings"
+)
+
+func main() {
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Request a greeting message.
+	message, err := greetings.Hello("")
+	// If an error was returned, print it to the console and exit the program
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message to console
+	fmt.Println(message)
+}
